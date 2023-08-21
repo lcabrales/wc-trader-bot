5aafba71fbbe8ec2a5a5f6523449a04583ad21f5
lcabrales <lcabrales@hastexp.com>
2023-08-21 22:16:07 +0100

    Implement user clear command

--- wc_trader_bot/library/cogs/admin.py
+++ wc_trader_bot/library/cogs/admin.py
@@ -5,13 +5,27 @@ from aiohttp import request
 from discord import Member, Embed
 from discord.ext.commands import Cog
 from discord.ext.commands import BadArgument, BucketType, CheckFailure
-from discord.ext.commands import command, has_permissions
+from discord.ext.commands import command, has_permissions, group
 
 from ..db import db
 
+
+COLOUR_DEFAULT = 0x0058F2
+
+
+USER_REMOVE_ALL_PIECES_QUERY = """
+DELETE FROM user_piece
+WHERE user_id = ?
+AND piece_id IN (
+    SELECT id
+    FROM piece
+);
+"""
+
+
 class Admin(Cog):
-	def __init__(self, bot):
-		self.bot = bot
+	def __init__(self, client):
+		self.client = client
 
 	@command(name="prefix")
 	@has_permissions(manage_guild=True)
@@ -28,11 +42,61 @@ class Admin(Cog):
 		if isinstance(exc, CheckFailure):
 			await ctx.send("You need the Manage Server permission to do that.")
 
+	@group(invoke_without_command=True)
+	@has_permissions(manage_guild=True)
+	async def user(self, ctx):
+		await ctx.send(f'Please specify the user command')
+
+	@user.error
+	async def user_error(self, ctx, exc):
+		if isinstance(exc, CheckFailure):
+			await ctx.send("You need the Manage Server permission to do that.")
+
+	@user.command(name="clear")
+	@has_permissions(manage_guild=True)
+	async def clear_user(self, ctx, member: Member):
+		if member is None:
+			await ctx.send(f'Bad argument: {member}')
+			raise BadArgument
+		
+		await ctx.send("Are you sure you want to run this command? (yes/no)")
+
+		def check(m): # checking if it's the same user and channel
+			return m.author == ctx.author and m.channel == ctx.channel
+
+		try: # waiting for message
+			response = await self.client.wait_for('message', check=check, timeout=30.0) # timeout - how long bot waits for message (in seconds)
+		except TimeoutError: # returning after timeout
+			return
+
+		# if response is different than yes / y - return
+		if response.content.lower() not in ("yes", "y"):
+			return
+		
+		# confirmation was provided, proceed to remove the pieces
+
+		db.execute(USER_REMOVE_ALL_PIECES_QUERY, member.id)
+
+		embed_title = "Clear user data"
+		embed = Embed(
+			title=embed_title, 
+			description=f"Cleared all user data belonging to **{member.name} ({member.id})**", 
+			colour=COLOUR_DEFAULT, 
+			timestamp=None
+		)
+		
+		await ctx.send(embed=embed)
+
+	@clear_user.error
+	async def clear_user_error(self, ctx, exc):
+		if isinstance(exc, CheckFailure):
+			await ctx.send("You need the Manage Server permission to do that.")
+
 	@Cog.listener()
 	async def on_ready(self):
-		if not self.bot.ready:
-			self.bot.cogs_ready.ready_up("admin")
+		if not self.client.ready:
+			self.client.cogs_ready.ready_up("admin")
 
 
-async def setup(bot):
-	await bot.add_cog(Admin(bot))
+async def setup(client):
+	await client.add_cog(Admin(client))
